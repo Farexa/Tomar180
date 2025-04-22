337ae7fad907b83baa2929ac7802d4feb67bceb9
gameyall232 <50255383+nullMeansNull@users.noreply.github.com>
2025-04-21 22:44:25 -0700

    bug fix

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -3,14 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class EndScreen : MonoBehaviour
 {
-    public void PlayButtonPressed(int sceneIndex)
-    {
-        SceneManager.LoadScene(sceneIndex);
-    }
+	 public void PlayButtonPressed(int sceneIndex)
+	 {
+		  SceneManager.LoadScene(sceneIndex);
+		PlayerController.wumpaFruit = 0;
+	 }
 
-    public void QuitButtonPressed()
-    {
-        print("Quit Game");
-        Application.Quit();
-    }
+	 public void QuitButtonPressed()
+	 {
+		  print("Quit Game");
+		  Application.Quit();
+	 }
 }
